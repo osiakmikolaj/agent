19c8e3a7338f5ca2b7dd0b8ed4bf4b668f61b0e9
Mikołaj Osiak <osiakmikolaj@gmail.com>
2025-07-18 19:47:07 +0200

    Added hardcoded system prompt

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory=None):
     try:
@@ -49,3 +50,17 @@ def get_size(start_path):
                 total_size += os.path.getsize(fp)
 
     return total_size
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 # get gemini key from env
 load_dotenv()
@@ -32,13 +33,48 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
 
-# response from the model
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+# Hardcoded system prompt
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info
+    ]
 )
 
-# printing out the response and meta data
-print(response.text)
+config = types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
+
+# response from the model
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=messages,
+    config=config
+)
+
+# Helper function
+def get_function_call(response):
+    try:
+        return response.candidates[0].content.parts[0].function_call
+    except (IndexError, AttributeError):
+        return None
+
+# Check for a function call
+function_call_part = get_function_call(response)
+if function_call_part:
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print("No function call found in the response.")
+    print(response.text)
 
 # only print if --verbose flag is included
 if if_verbose:
